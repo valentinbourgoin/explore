01518436ccc6cde6615d1aa0565dcc2e3e7fb040
Valentin Bourgoin <valentin.bourgoin@gmail.com>
2021-09-14 13:09:23 +0200

    Force postgis in Heroku settings

--- explore/settings.py
+++ explore/settings.py
@@ -100,6 +100,7 @@ DATABASES = {
 if os.environ.get('ENV') == 'PRODUCTION':
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+    DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis',
 
 
 # Password validation
